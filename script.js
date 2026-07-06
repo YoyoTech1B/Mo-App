156f40b97e7a31ab39d04edf176e878e0cb62b2b
YoyoTech1B <yoyoblue30@gmail.com>
2026-07-06 22:56:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const nav = document.querySelector("nav");
 
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+        nav.classList.add("scrolled");
+    }else{
+        nav.classList.remove("scrolled");
+    }
+
+});
